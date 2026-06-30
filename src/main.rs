2b67450f4d8a39dd8e52f12329489d704b88238c
Rique-Notebook-Acer <rique9906@gmail.com>
2026-06-30 16:24:21 -0300

    primeiro commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // roboBomDia();
+
+    
+    let mut x: i32 = 5;
+    x += 6;
+    
+    println!("Hello, world! {}", x);
+
+}
+
+// fn saudacao() {
+//     println!("Oi, bom dia!\nComo você está?");
+// }
+
+// fn roboBomDia(){
+//     saudacao();
+// }
