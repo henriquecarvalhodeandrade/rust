8c1d26974ac766013fb62d8d1eeebc167abba2e1
rique-notebook-acer-aspire-e-14 <rique9906@gmail.com>
2026-07-04 12:49:32 -0300

    Exercicio 1 de codicionais

---
+++ exercises/conditions and loops/ex01/src/main.rs
@@ -0,0 +1,16 @@
+// Exercício 1 (Fácil)
+// Desenvolva um programa que declare uma variável inteira contendo um número qualquer.
+// Utilize uma estrutura if-else para verificar se esse número é positivo, negativo ou
+// igual a zero. Ao final, exiba uma mensagem indicando a classificação do número.
+
+fn main() {
+    let number:i32 = 10;
+
+    if (number  == 0) {
+        println!("O número inserido é 0!");
+    } else if (number < 0){
+        println!("O número inserido é negativo.");
+    } else {
+        println!("O número inserido é positivo.");
+    }
+}
